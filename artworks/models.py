aad9668f2dee1c64368a83da518eda7bfab97264
Amanda Koka <amandakoka@icloud.com>
2024-07-03 14:32:11 +0000

    fix: Add class meta to fix category spelling in admin.

--- artworks/models.py
+++ artworks/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):

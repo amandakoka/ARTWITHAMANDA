390bdd3fd8cf7ef362e0a108cb504fcb05336421
Amanda Koka <amandakoka@icloud.com>
2024-07-03 18:51:49 +0000

    feat: Add basic structure of art details page and views.

--- artworks/models.py
+++ artworks/models.py
@@ -2,15 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+
 class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-        
+
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
+
 
 class Artwork(models.Model):
     name = models.CharField(max_length=200)
--- artworks/views.py
+++ artworks/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Artwork
+
 
 # Create your views here.
 def all_artworks(request):
@@ -8,7 +9,19 @@ def all_artworks(request):
     artworks = Artwork.objects.all()
 
     context = {
-        'artworks' : artworks
+        'artworks': artworks
     }
 
     return render(request, 'artworks/artworks.html', context)
+
+
+def artwork_detail(request, artwork_id):
+    """ View to show one artwork """
+
+    artwork = get_object_or_404(Artwork, pk=artwork_id)
+
+    context = {
+        'artwork': artwork
+    }
+
+    return render(request, 'artworks/artwork_detail.html', context)

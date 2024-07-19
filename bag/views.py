6794cae746f707b40604889d861631bbe1ab5e17
Amanda Koka <amandakoka@icloud.com>
2024-07-19 11:27:18 +0000

    feat: Add messages for adding adjusting and removing from bag .

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 
 from artworks.models import Artwork
@@ -12,13 +12,14 @@ def view_bag(request):
 def add_to_bag(request, artwork_id):
     """ Add a quantity of the specified artwork to the shopping bag """
 
-    artwork = Artwork.objects.get(pk=artwork_id)
+    artwork = get_object_or_404(Artwork, pk=artwork_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if artwork_id in list(bag.keys()):
         bag[artwork_id] += quantity
+        messages.success(request, f'Updated {artwork.name} quantity to {bag[artwork_id]}')
     else:
         bag[artwork_id] = quantity
         messages.success(request, f'Added {artwork.name} to your bag')
@@ -29,23 +30,31 @@ def add_to_bag(request, artwork_id):
 
 def adjust_bag(request, artwork_id):
     """ Adjust the quantity of the specified artwork in the shopping bag """
+
+    artwork = get_object_or_404(Artwork, pk=artwork_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[artwork_id] = quantity
+        messages.success(request, f'Updated {artwork.name} quantity to {bag[artwork_id]}')
     else:
         bag.pop(artwork_id, None)
+        messages.success(request, f'Removed {artwork.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
 
 def remove_from_bag(request, artwork_id):
+
+    artwork = get_object_or_404(Artwork, pk=artwork_id)
     bag = request.session.get('bag', {})
 
     if artwork_id in bag:
         bag.pop(artwork_id)
         request.session['bag'] = bag
+        messages.success(request, f'Removed {artwork.name} from your bag')
 
     return redirect(reverse('view_bag'))
+

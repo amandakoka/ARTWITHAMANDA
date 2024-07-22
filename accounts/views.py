140ababfcf04511aa64260ca112ffd4896658c1e
Amanda Koka <amandakoka@icloud.com>
2024-07-22 20:24:50 +0000

    feat: Add account form, views and updated template.

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserAccount
+from .forms import UserAccountForm
 
 # Create your views here.
 
@@ -8,9 +10,20 @@ def account(request):
     """ Display the user's account. """
     account = get_object_or_404(UserAccount, user=request.user)
 
+    if request.method == 'POST':
+        form = UserAccountForm(request.POST, instance=account)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserAccountForm(instance=account)
+    orders = account.orders.all()
+
     template = 'accounts/account.html'
     context = {
-        'account': account,
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)

b3f2eb5bff8ae5fa4cf746c0f4dd393f661043d9
Amanda Koka <amandakoka@icloud.com>
2024-07-22 12:18:21 +0000

    feat: Add basic checkout functionality.

--- art_with_amanda/settings.py
+++ art_with_amanda/settings.py
@@ -169,9 +169,12 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Delivery configuration
+# Stripe settings
 STANDARD_DELIVERY_PERCENTAGE = 0
 FREE_DELIVERY_THRESHOLD = Decimal('0.00')
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

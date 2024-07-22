e0f15ae83b1ec4b4935ea4722b216daf09b503ca
Amanda Koka <amandakoka@icloud.com>
2024-07-22 11:10:00 +0000

    feat: Add stripe elements.

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PfJtZHul12GLcCBxmzVIvALBAdOc2LUHH7NofrIjlAmdoz3yluWSjB8Y1Cc1RlxTCBV1xmNIQQCglyLmB71C2w900KeSZXJcc',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

2424812b8f041067aea9ed1920d5445e3727ac80
Amanda Koka <amandakoka@icloud.com>
2024-07-24 10:57:09 +0000

    add runtime.txt

--- art_with_amanda/settings.py
+++ art_with_amanda/settings.py
@@ -10,13 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
-
 import os
 if os.path.exists("env.py"):
     import env
 
 import dj_database_url
+
+from pathlib import Path
 from decimal import Decimal
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
 

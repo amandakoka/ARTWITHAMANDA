4d8257def8d92043469c0b6fc0d80b1b5e8adf67
Amanda Koka <amandakoka@icloud.com>
2024-07-26 12:50:38 +0000

    Fix remove bag url.

--- bag/urls.py
+++ bag/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add_to_bag/<int:artwork_id>/', views.add_to_bag, name='add_to_bag'),
     path('adjust/<int:artwork_id>/', views.adjust_bag, name='adjust_bag'),
-    path('remove/<int:artwork_id>/', views.remove_from_bag, name='remove_from_bag'),
+    path('remove/<artwork_id>/', views.remove_from_bag, name='remove_from_bag'),
 ]
